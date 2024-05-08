b685a426d38412c9cd28dd73ff17091e035e4829
SummerSamara2853 <145475928+SummerSamara2853@users.noreply.github.com>
2024-05-08 11:03:54 -0400

    Add files via upload

---
+++ player.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("coin-sprite.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+
